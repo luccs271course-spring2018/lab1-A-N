975081c47c7293e87cdd0417f78c7aed0b0bb96f
Fischer <afischer5@luc.edu>
2017-09-01 09:50:51 -0500

    testing

---
+++ IdeaProjects/lab1/src/main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by afischer5 on 9/1/2017.
+ */
+
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args){
+
+        int input;
+        Scanner keyboard = new Scanner(System.in);
+
+        input=keyboard.nextInt();
+
+        for(int i=0; i<input; i++){
+            if(i%3 == 0 ) System.out.print("Fizz ");
+            if(i%5 == 0 )  System.out.print("Buzz");
+            if(i%3 != 0 && i%5 != 0) System.out.print(i);
+            System.out.println("");
+        }
+    }
+}
